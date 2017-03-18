2cfe4a561c6d53bf9c4678245199c544440675ff
Wilmer Blanco <wilmervzla2000@gmail.com>
2017-03-17 23:37:22 -0300

    agregado plugin de own-carrousel

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
 	var slider_project = $('.js_carousel_project');
-	slider_project.owlCarousel({items:1});
+	slider_project.owlCarousel({items:1,margin:0,autoWidth:true});
 });
