b8bc9763bf53dcec5353cf196e224ff3e81edad2
Wilmer Blanco <wilmervzla2000@gmail.com>
2017-04-03 23:36:23 -0300

    cambios

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	var slider_project = $('.js_carousel_project');
-	slider_project.owlCarousel({items:1,margin:0,autoWidth:true});
+	slider_project.owlCarousel({items:1,margin:0,autoWidth:true,loop:true,responsive:true,autoplay:true});
+
 	$('.carousel_project .js_controls a').on('click', function(event){
 		event.preventDefault();
 		if ($(this).attr('data-diretion') == 'next'){
